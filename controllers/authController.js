3a78cd3f692471f6ac886acf20d5626a469e01bb
Bill Bakopoulos <billbakopoulos@Bills-MacBook-Air.local>
2019-06-03 11:37:48 -0500

    test login route successfully hit from react application

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,6 +4,9 @@ const User = require('../models/user.js');
 const bcrypt = require('bcryptjs');
 
 
+router.get('/login', async (req,res,next) => {
+	console.log('hey, front and back end are connected');
+})
 
 
 
